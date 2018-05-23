a4ba1928dcb3505acb9f3b98b648f0e5642d3fff
Jebediah Elliott <jebediahelliott@gmail.com>
2018-05-23 02:23:36 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,5 +15,5 @@ end
 
 def find_the_cheese(string)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  string.find{ |string| string == cheese_types.any?}
+  string.find{ |string| string == "cheddar" || string == "gouda" || string == "camembert"}
 end
