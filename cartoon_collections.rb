56e044489746f76d4aa9b135bb244188321c3947
Jebediah Elliott <jebediahelliott@gmail.com>
2018-05-23 02:15:49 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,8 +9,8 @@ def summon_captain_planet(calls)
   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(strings)
+  strings.any?{ |string| string.length > 4}
 end
 
 def find_the_cheese# code an argument here
